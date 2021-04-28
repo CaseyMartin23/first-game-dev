d907018f447eb55a92d5bc21814d4e1e667a3a34
CaseyMartin23 <cm232311@gmail.com>
2021-04-28 17:50:25 +0200

    Added point system

--- game/script.js
+++ game/script.js
@@ -8,6 +8,8 @@ canvas.height = 600;
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
+const defenders = [];
+let numberOfResources = 300;
 
 // mouse
 const mouse = {
@@ -68,21 +70,76 @@ const handleGameGrid = () => {
 
 // projectiles
 // defenders
+class Defender {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = cellSize;
+    this.height = cellSize;
+    this.shooting = false;
+    this.health = 100;
+    this.projectiles = [];
+    this.timer = 0;
+  }
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = "gold";
+    ctx.font = "30px Arial";
+    ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
+  }
+}
+
+canvas.addEventListener("click", () => {
+  const gridPositionX = mouse.x - (mouse.x % cellSize);
+  const gridPositionY = mouse.y - (mouse.y % cellSize);
+  if (gridPositionY < cellSize) return;
+
+  for (let index = 0; index < defenders.length; index++) {
+    if (
+      defenders[index].x === gridPositionX &&
+      defenders[index].y === gridPositionY
+    ) {
+      return;
+    }
+  }
+
+  let defenderCost = 100;
+  if (numberOfResources >= defenderCost) {
+    defenders.push(new Defender(gridPositionX, gridPositionY));
+    numberOfResources -= defenderCost;
+  }
+});
+
+const handleDefenders = () => {
+  for (let index = 0; index < defenders.length; index++) {
+    defenders[index].draw();
+  }
+};
+
 // enemies
 // resources
 // utilities
-function animate() {
+const handleGameStatus = () => {
+  ctx.fillStyle = "gold";
+  ctx.font = "30px Arial";
+  ctx.fillText(`Resources: ${numberOfResources}`, 20, 55);
+};
+
+const animate = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "blue";
   ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
 
   handleGameGrid();
+  handleDefenders();
+  handleGameStatus();
 
   window.requestAnimationFrame(animate);
-}
+};
 animate();
 
-function collision(first, second) {
+const collision = (first, second) => {
   if (
     !(
       first.x > second.x + second.width ||
@@ -92,4 +149,4 @@ function collision(first, second) {
     )
   )
     return true;
-}
+};
