5b675c5f442f41e7018da52f73393aa1fef171a2
CaseyMartin23 <cm232311@gmail.com>
2021-04-28 02:17:36 +0200

    Collision detection added

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import express from "express";
-import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 app.use(express.static("game"));
 
