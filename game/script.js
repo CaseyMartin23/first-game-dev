77947bb2735bf7ee9823ceac522754b7b92be840
CaseyMartin23 <cm232311@gmail.com>
2021-05-02 15:51:35 +0200

    Added enemy class

--- game/script.js
+++ game/script.js
@@ -118,6 +118,29 @@ const handleDefenders = () => {
 };
 
 // enemies
+class Enemy {
+  constructor(verticalPosition) {
+    this.x = canvas.width;
+    this.y = verticalPosition;
+    this.width = cellSize;
+    this.height = cellSize;
+    this.speed = Math.random() * 0.2 + 0.4;
+    this.movement = this.speed;
+    this.health = 100;
+    this.maxHealth = this.health;
+  }
+  update() {
+    this.x -= this.movement;
+  }
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = "black";
+    ctx.font = "30px Arial";
+    ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
+  }
+}
+
 // resources
 // utilities
 const handleGameStatus = () => {
